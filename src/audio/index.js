890f23b272d111f5ef0a3729dca6e4e8b0d9923d
connorwalsh <c@polygon.pizza>
2021-01-18 12:40:01 -0500

    introduces gain node to reduce distortions

--- src/audio/index.js
+++ src/audio/index.js
@@ -2,18 +2,28 @@ import { audioContext } from '../context/audio'
 
 export class AudioEngine {
     constructor() { 
-        
-        
+        // ADDED A GAIN NODE.
+        // its important to reduce the gain of the output of both oscillators
+        // when the resulting signal is their superposition. this is due to the
+        // fact that their amplitudes are summed and sometimes above 1 and below -1.
+        // but if we reduce the overall amplitude (with a gain node), we won't experience
+        // clipping/distortion.
+        //
+        // try messing around with different gain values!
+        this.gain = audioContext.createGain()
+        this.gain.gain.value = 0.5
+        this.gain.connect(audioContext.destination)
     }
 
-    play() {
-        this.freqs = [220.0, 275.0]
+    play() {                
+        this.freqs = [800.0, 440.0]
         this.oscs = new Array(this.freqs.length)
         for(var i = 0; i < this.freqs.length; i++){
             this.oscs[i] = audioContext.createOscillator()
             this.oscs[i].type = 'sine'
             this.oscs[i].frequency.value = this.freqs[i]
-            this.oscs[i].connect(audioContext.destination)
+            this.oscs[i].connect(this.gain)
+            
             this.oscs[i].start()
          }
 
