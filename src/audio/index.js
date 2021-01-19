d92c1d160136859b04d385baf6a7e8e98222d373
tjsewell <tjsewell@gmail.com>
2021-01-18 21:35:38 -0500

    sum tweaks

--- src/audio/index.js
+++ src/audio/index.js
@@ -11,11 +11,11 @@ export class AudioEngine {
         //
         // try messing around with different gain values!
         this.fund = 56 // sets frequency of first oscillator
-        this.freqs = [1,2,3,4,5,6,7,8] //frequency ratios
+        this.freqs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24] //frequency ratios
         this.freqs = this.freqs.map(x => x*this.fund/this.freqs[0]) //absolute frequencies
 
 
-        this.gainvals = this.freqs.map(x => 1 / x) //sets individual gains 1/f
+        this.gainvals = this.freqs.map(x => Math.pow(x,-1.5)) //sets individual gains 1/f
         this.gain = audioContext.createGain()
         this.gain.gain.value = 0.8 / this.gainvals.reduce((a,b) => a+b) //sets master gain to a/(sum of individual)
         this.gain.connect(audioContext.destination)
@@ -34,9 +34,9 @@ export class AudioEngine {
             this.gainz[i].connect(this.gain)
             
             //starts oscillators at random time
-            this.spread = 50 //size of phase randomization window in ms
-            this.delay = 0.0 //time between successive notes in s
-            this.oscs[i].start(audioContext.currentTime + Math.random()*this.spread/1000 + this.delay*i)
+            this.spread = 1/this.fund //size of phase randomization window in ms
+            this.delay = 0.05//time between successive notes in s
+            this.oscs[i].start(audioContext.currentTime + Math.random()*this.spread/1000.0 + this.delay*i)
             
          }
 
