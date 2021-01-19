8171bdeb76011861ea627bbb1f17c9c9e819fa87
tjsewell <tjsewell@gmail.com>
2021-01-19 16:57:19 -0500

    frequency sampling according to arbitrary powerlaw (including log)

--- src/audio/index.js
+++ src/audio/index.js
@@ -12,19 +12,34 @@ export class AudioEngine {
         // try messing around with different gain values!
         this.gain = audioContext.createGain()
         this.gain.connect(audioContext.destination)
+
+
+        //set min and max frequencies for oscillator distribution
+        this.fmin = 60
+        this.fmax = 1920 
+        //set power law for frequency distribution, 
+        //1 is linear(more top heavy, equal across frequencies)
+        //0 is log (more bottom heavy, equal across octaves)
+        this.alpha = 0
     }
 
-    play() {             
-        this.freqmin = 40
-        this.freqmax = 2048  
+    g(){
+        if(this.alpha == 0){
+            return Math.pow(Math.E,Math.random()*(Math.log(this.fmax)-Math.log(this.fmin))+Math.log(this.fmin))
+        } else {
+            return Math.pow(Math.random()*(Math.pow(this.fmax,this.alpha)-Math.pow(this.fmin,this.alpha))+Math.pow(this.fmin,this.alpha),1/this.alpha)
+        }
 
-        //selects frequencies uniformly on log scale 
-        this.freqs = Array(200).fill().map(() => Math.pow(Math.E,Math.random()*(Math.log(this.freqmax)-Math.log(this.freqmin))+Math.log(this.freqmin))) //absolute frequencies
+    }
+
+
+    play() {             
+         
+
+        //selects frequencies according to powerlaw distribution g()
+        this.freqs = Array(200).fill().map(() => this.g()) 
         
-        //selects frequecys uniformly on linear scale
-        //this.freqs = Array(200).fill().map(() => Math.random()*(this.freqmax-this.freqmin)+this.freqmin) //absolute frequencies
-        
-        this.gainvals = this.freqs.map(f => Math.pow(f,-2)) //sets individual gains f^n
+        this.gainvals = this.freqs.map(f => Math.pow(f,-1)) //sets individual gains f^n
         this.gain.gain.value = 1 / this.gainvals.reduce((a,b) => a+b) //sets master gain to a/(sum of individual)
         this.oscs = new Array(this.freqs.length)
         this.gainz = new Array(this.freqs.length)
@@ -38,7 +53,7 @@ export class AudioEngine {
             this.gainz[i].connect(this.gain)
             
             //starts oscillators at random time
-            this.spread = 100 //size of phase randomization window in ms
+            this.spread = 50 //size of phase randomization window in ms
             this.delay = 0.0 //time between successive notes in s
             this.oscs[i].start(audioContext.currentTime + Math.random()*this.spread/1000.0 + this.delay*i)
             
